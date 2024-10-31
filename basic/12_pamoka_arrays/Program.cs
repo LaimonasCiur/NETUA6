ec674bf9e11aaef7a50e5dc5bd3e2c2676d7c46d
Laimonas_Ciurlionis <laimonas.ciurlionis@gmail.com>
2024-10-31 19:11:46 +0200

    up

---
+++ basic/12_pamoka_arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] emptyArray = new int[10];
+int[] arrayWithInitialValues = { 1, 2, 88, 105, 999 };
+
+Console.WriteLine(emptyArray[5]);
+
+arrayWithInitialValues[4] = 23;
+
+string[] sentences = { "abc", "text", "random" };
+for (int i = 0; i < sentences.Length; i++) 
+{
+    Console.WriteLine(sentences[i]);
+}
+
+for (int i = 0; i < emptyArray.Length; i++) 
+{
+    emptyArray[i] = i * i;
+    Console.WriteLine(emptyArray[i]);
+}
+
+
+double avarage = Methods.CaculateAvg(new[] { 1, 5, 6, });
+
+
+public static class Methods 
+{
+    public static double CaculateAvg(int[] numbers) 
+    {
+        double sum = 0;
+
+        foreach (int number in numbers) 
+        { 
+            sum += number;
+        }
+
+        return sum / numbers.Length;
+    }
+}
+
---
+++ basic/14_pamoka_List/Program.cs
@@ -0,0 +1,50 @@
+﻿List<int> numbers = new List<int>();
+
+List<int> numberWithValues = new List<int> { 99, 101, 642, 1 };
+
+var myVarNumbers = new List<int>();
+
+var numberVarWithValues = new List<int> { 99, 101, 642 };
+var myInt = 0;
+var t = "";
+
+numberWithValues.Add(753);
+
+Console.WriteLine("Add");
+foreach (var number in numberWithValues) 
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("Remove");
+numberWithValues.Remove(99);
+foreach (var number in numberWithValues)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("Count");
+int count = numberWithValues.Count;
+Console.WriteLine(count);
+
+Console.WriteLine("Sort");
+numberWithValues.Sort();
+foreach (var number in numberWithValues)
+{
+    Console.WriteLine(number);
+}
+
+
+
+List<object> myObjectList = new List<object>();
+myObjectList.Add(1);
+myObjectList.Add("myString");
+myObjectList.Add('n');
+
+var t2 = myObjectList[0];
+Console.WriteLine();
+
+
+
+
+
